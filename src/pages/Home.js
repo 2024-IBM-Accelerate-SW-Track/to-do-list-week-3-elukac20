6b20a11b0291ae45c76ab2ed04e2018bc345d84b
elukac20 <141880386+elukac20@users.noreply.github.com>
2024-06-19 23:07:02 -0400

    doesn't add invalid dates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -30,6 +30,9 @@ class Home extends Component {
   addTodo = (todo) => {
     const exists = this.state.todos.find(t => t.content === todo.content);
     if (exists){ return }
+    if(todo.due === null || todo.due === "Invalid Date"){
+      return;
+    }
     // In React, keys or ids in a list help identify which items have changed, been added or removed. Keys
     // should not share duplicate values.
     // To avoid having dup values, we use the Math.random() function to generate a random value for a todo id.
@@ -39,14 +42,10 @@ class Home extends Component {
     // Create a array that contains the current array and the new todo item
     let new_list = [...this.state.todos, todo];
     // Update the local state with the new array.
-    
-    if (null in new_list | "Invalid Date" in new_list) {
-      return
-      } else {
-        this.setState({
+    this.setState({
           todos: new_list,
-        });
-      }
+    });
+    
   };
   render() {
     return (
